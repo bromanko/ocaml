652121706f8e1ad1cf155403e31d072823082343
Brian Romanko <hello@bromanko.com>
2024-05-02 15:30:03 -0700

    Add live reload

--- bin/main.ml
+++ bin/main.ml
@@ -4,6 +4,6 @@ let hello who =
   html [] [ body [] [ h1 [] [ txt "Hello, %s!" who ] ] ]
 
 let () =
-  Dream.run @@ Dream.logger
+  Dream.run @@ Dream.logger @@ Dream.livereload
   @@ Dream.router
        [ Dream.get "/" (fun _ -> Dream_html.respond (hello "world")) ]
