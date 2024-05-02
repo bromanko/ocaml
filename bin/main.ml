eb6161b347efd9a0a221cf64628d4a91681455bc
Brian Romanko <hello@bromanko.com>
2024-05-02 12:57:44 -0700

    Add hello world web server

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,9 @@
-let () = print_endline "Hello, World!"
+let hello who =
+  let open Dream_html in
+  let open HTML in
+  html [] [ body [] [ h1 [] [ txt "Hello, %s!" who ] ] ]
+
+let () =
+  Dream.run @@ Dream.logger
+  @@ Dream.router
+       [ Dream.get "/" (fun _ -> Dream_html.respond (hello "world")) ]
