e6553fd390a7ef6ac25841cde18bd47795492888
Brian Romanko <hello@bromanko.com>
2024-05-04 12:11:19 -0700

    Add command to start server

--- bin/main.ml
+++ bin/main.ml
@@ -1,18 +1,18 @@
 open! Core
 open Cmdliner
 
-let revolt () = Stdio.print_endline "Revolt!"
-let revolt_t = Term.(const revolt $ const ())
-let cmd = Cmd.v (Cmd.info "server") revolt_t
+let hello who =
+  let open Dream_html in
+  let open HTML in
+  html [] [ body [] [ h1 [] [ txt "Hello, %s!" who ] ] ]
 
-(* let hello who =
-   let open Dream_html in
-   let open HTML in
-   html [] [ body [] [ h1 [] [ txt "Hello, %s!" who ] ] ] *)
+let start_server () =
+  Dream.run @@ Dream.logger @@ Dream.livereload
+  @@ Dream.router
+       [
+         Dream.get "/" (fun _ -> Dream_html.respond (hello "world of the foo!"));
+       ]
 
+let start_server_t = Term.(const start_server $ const ())
+let cmd = Cmd.v (Cmd.info "server") start_server_t
 let () = exit (Cmd.eval cmd)
-(* Dream.run @@ Dream.logger @@ Dream.livereload
-   @@ Dream.router
-        [
-          Dream.get "/" (fun _ -> Dream_html.respond (hello "world of the foo!"));
-        ] *)
