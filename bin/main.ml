28b948a69875d72cd928c1607f1bc69670fc6b8d
Brian Romanko <hello@bromanko.com>
2024-05-04 12:14:45 -0700

    Add port option to cli

--- bin/main.ml
+++ bin/main.ml
@@ -6,13 +6,26 @@ let hello who =
   let open HTML in
   html [] [ body [] [ h1 [] [ txt "Hello, %s!" who ] ] ]
 
-let start_server () =
-  Dream.run @@ Dream.logger @@ Dream.livereload
+let start_server port =
+  Dream.run ~port @@ Dream.logger @@ Dream.livereload
   @@ Dream.router
        [
          Dream.get "/" (fun _ -> Dream_html.respond (hello "world of the foo!"));
        ]
 
-let start_server_t = Term.(const start_server $ const ())
-let cmd = Cmd.v (Cmd.info "server") start_server_t
-let () = exit (Cmd.eval cmd)
+let port =
+  let env =
+    let doc = "Overrides the port." in
+    Cmd.Env.info "SERVER_PORT" ~doc
+  in
+  let doc = "Port to listen on" in
+  Arg.(value & opt int 8080 & info [ "p"; "port" ] ~env ~docv:"PORT" ~doc)
+
+let cmd =
+  let info =
+    let doc = "Dream server" in
+    Cmd.info "server" ~version:"%‌%VERSION%%" ~doc
+  in
+  Cmd.v info Term.(const start_server $ port)
+
+let () = Cmd.eval cmd |> exit
