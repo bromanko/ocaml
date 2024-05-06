3b13af48e36d1b97313ce60f4db75528390e1ec5
Brian Romanko <hello@bromanko.com>
2024-05-05 20:29:03 -0700

    Simplify href url setting

--- src/templates/layouts/layout.ml
+++ src/templates/layouts/layout.ml
@@ -1,3 +1,5 @@
+let href path = Dream_html.uri_attr "href" "%s" (Static_files.Asset.url path)
+
 let base inner =
   let open Dream_html in
   let open HTML in
@@ -9,14 +11,9 @@ let base inner =
           meta [ charset "utf-8" ];
           meta
             [ name "viewport"; content "width=device-width, initial-scale=1.0" ];
-          link
-            [
-              rel "stylesheet";
-              (* href "css/main.css" *)
-              href "%s" (Static_files.Asset.url "css/main.css");
-            ];
+          link [ rel "stylesheet"; href "css/main.css" ];
         ];
-      body [ class_ "p-8" ] [ inner ];
+      body [ class_ "bg-red-500 p-8" ] [ inner ];
     ]
 
 let render = base
