ec6f7fecd46297ded0372ade32b223af7328b64c
Brian Romanko <hello@bromanko.com>
2024-05-08 13:19:06 -0700

    Fix clobberring of nodePackages
    The daisyUI overlay was replacing the entire nodePackages object.

--- nix/overlays/daisyui/default.nix
+++ nix/overlays/daisyui/default.nix
@@ -1,27 +1,29 @@
 self: super: {
-  nodePackages.daisyui = super.buildNpmPackage rec {
-    pname = "daisyui";
-    version = "4.11.1";
+  nodePackages = super.nodePackages // {
+    daisyui = super.buildNpmPackage rec {
+      pname = "daisyui";
+      version = "4.11.1";
 
-    src = super.fetchFromGitHub {
-      owner = "saadeghi";
-      repo = pname;
-      rev = "v${version}";
-      hash = "sha256-ZrlqLSXiaO4+FMR0OzD1QCFQHbfCdypMgHoD4jVTlRc=";
-    };
+      src = super.fetchFromGitHub {
+        owner = "saadeghi";
+        repo = pname;
+        rev = "v${version}";
+        hash = "sha256-ZrlqLSXiaO4+FMR0OzD1QCFQHbfCdypMgHoD4jVTlRc=";
+      };
 
-    npmDepsHash = "sha256-IwJiuMrFkZlRqZ4Ie9UKt1oT+VXBQyMNoNceUb+XcNQ=";
-    postPatch = ''
-      cp ${./package-lock.json} ./package-lock.json
-    '';
+      npmDepsHash = "sha256-IwJiuMrFkZlRqZ4Ie9UKt1oT+VXBQyMNoNceUb+XcNQ=";
+      postPatch = ''
+        cp ${./package-lock.json} ./package-lock.json
+      '';
 
-    npmPackFlags = [ "--ignore-scripts" ];
-    NODE_OPTIONS = "--openssl-legacy-provider";
+      npmPackFlags = [ "--ignore-scripts" ];
+      NODE_OPTIONS = "--openssl-legacy-provider";
 
-    meta = with super.lib; {
-      description = "A free and open-source Tailwind CSS component library ";
-      homepage = "https://daisyui.com/";
-      license = licenses.mit;
+      meta = with super.lib; {
+        description = "A free and open-source Tailwind CSS component library ";
+        homepage = "https://daisyui.com/";
+        license = licenses.mit;
+      };
     };
   };
 }
